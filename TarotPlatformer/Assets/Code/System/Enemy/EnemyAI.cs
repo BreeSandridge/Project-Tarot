dd9ac94607f8276ba48cd74248d242dee62d6a2a
unknown <ITP@OHE540-23F.vlab.usc.edu>
2019-07-03 15:47:38 -0700

    collision for square sprites

--- TarotPlatformer/Assets/Code/System/Enemy/EnemyAI.cs
+++ TarotPlatformer/Assets/Code/System/Enemy/EnemyAI.cs
@@ -2,24 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour { 
-    public GameObject obj;
-    Vector3 pos;
+public class EnemyAI : MonoBehaviour {
 
-    public void Enemy()
-
+    public enum State
     {
-        pos = transform.position;
+        Walk,
+        Attack,
+        Die,
+    }
 
-        if (Enemy.dir.Player == true)
-        {
-            pos += new Vector3(1, 0, 0);
+    public bool dir = true;
+
+    Rigidbody2D rb;
+    public State state;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        state = State.Walk;
+        
+    }
+
+
+    void Update()
+    {
+        //DrawBounds();
+        if (state == State.Walk) {
+            if (dir) {
+                rb.velocity = new Vector3(1, rb.velocity.y, 0);
+            } else {
+                rb.velocity = new Vector3(-1, rb.velocity.y, 0);
+            }
         }
-        else
-        {
-            pos += new Vector3(-1, 0, 0);
-        }
-      Instantiate(obj, pos, Quaternion.identity);
+    }
+
+
+    /*private void DrawBounds() {
+        float width = GetComponent<SpriteRenderer>().bounds.size.x;
+        float height = GetComponent<SpriteRenderer>().bounds.size.y;
+
+
+        //bottom
+        float botLeftX = transform.position.x - (width / 2);
+        float botRightX = transform.position.x + (width / 2);
+        float botY = transform.position.y - (height / 2);
+        DrawLine(new Vector3(botLeftX, botY, 0), new Vector3(botRightX, botY, 0), Color.cyan, .03f);
+
+    }*/
+
+    public void changeState()
+    {
+
+    }
+
+    private void walk() {
+
+    }
+
+    public void CollisionDetected(ChildCollider childScript)
+    {
+        print("child collided: " + childScript.name);
     }
 }
-
