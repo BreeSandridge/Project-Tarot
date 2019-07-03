3c627f028d8808d8c2ba4ea2ac326f64b2ff329b
unknown <ITP@OHE540-24F.vlab.usc.edu>
2019-07-03 11:47:48 -0700

    enemy

---
+++ TarotPlatformer/Assets/Code/System/Enemy/EnemyAI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour { 
+    public GameObject obj;
+    Vector3 pos;
+
+    public void Enemy()
+
+    {
+        pos = transform.position;
+
+        if (Enemy.dir.Player == true)
+        {
+            pos += new Vector3(1, 0, 0);
+        }
+        else
+        {
+            pos += new Vector3(-1, 0, 0);
+        }
+      Instantiate(obj, pos, Quaternion.identity);
+    }
+}
+
