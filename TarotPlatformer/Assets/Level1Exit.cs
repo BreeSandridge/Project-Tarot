9cdf451e07f1d66fe1322ab1ad14a4d892336071
unknown <ITP@OHE540-24F.vlab.usc.edu>
2019-07-10 11:58:37 -0700

    going into levels

---
+++ TarotPlatformer/Assets/Level1Exit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Go to level 1 Castle");
+        SceneManager.LoadScene("Level1Castle");
+       /* if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Level1Castle");
+        }*/
+    }
+}
---
+++ TarotPlatformer/Assets/Level2Exit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level2Exit : MonoBehaviour {
+
+    public string levelName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //Application.LoadLevel("BossRoom");
+            SceneManager.LoadScene("BossRoom");
+        }
+    }
+}
