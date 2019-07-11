c80022050a868d01b93523adfd045f134f1ce469
unknown <ITP@OHE540-24F.vlab.usc.edu>
2019-07-11 12:00:34 -0700

    FINAL BOSS

---
+++ TarotPlatformer/Assets/BossAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI : MonoBehaviour
+{
+    public GameObject BossFireball;
+    public GameObject fireballGun;
+    public float firstShotDelay;
+    public float fireRate;
+
+    string moveDirection = "up";
+   void Start()
+    {
+        InvokeRepeating("Shoot", firstShotDelay, fireRate);
+    }
+    void Shoot()
+
+    {
+        Instantiate(BossFireball, fireballGun.transform.position, transform.rotation);
+    }
+
+    private void Update()
+    {
+        //does moveDirection == up?
+        if(moveDirection == "up")
+        {
+            Transform.position.y += 90;
+
+
+            //move y position up
+
+            //has the boss reached the max y position?
+            if ()
+            {
+                moveDirection = "down";
+            }
+        }
+
+        transform.y()
+    }
+}
---
+++ TarotPlatformer/Assets/Code/System/Boss/BossManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour {
+    public float health = 200f;
+    public float damage = 10f;
+    public float atk_speed = 8f;
+    public float speed = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
